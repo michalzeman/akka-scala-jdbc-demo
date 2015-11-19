5a04f4ff4f56cb912aecede3c0051fbdbc8c24ce
Michal Zeman <zemo@Michals-MacBook-Pro.local>
2015-11-19 11:06:15 +0100

    Action actor change calling

--- src/main/scala/com/mz/example/actors/actions/UserActionActor.scala
+++ src/main/scala/com/mz/example/actors/actions/UserActionActor.scala
@@ -1,16 +1,13 @@
 package com.mz.example.actors.actions
 
 import com.mz.example.actors.jdbc.JDBCConnectionActor._
-import akka.actor.{PoisonPill, Actor, ActorLogging}
+import akka.actor.{ActorRef, PoisonPill, Actor, ActorLogging}
 import akka.util.Timeout
 import com.mz.example.actors.jdbc.JDBCConnectionActor
 import com.mz.example.actors.repositories.{AddressRepositoryActor, UserRepositoryActor}
 import com.mz.example.actors.services.UserServiceActor.{UserRegistrated, RegistrateUser}
 import com.mz.example.actors.services.{UserServiceActor, AddressServiceActor}
-import akka.pattern._
-import scala.concurrent._
 import scala.concurrent.duration._
-import scala.util.{Failure, Success}
 
 /**
  * Created by zemo on 27/10/15.
@@ -28,26 +25,43 @@ class UserActionActor extends Actor with ActorLogging {
   val userService = context.actorOf(UserServiceActor.props(userRepositoryProps, addressService))
   context.watch(userService)
 
+  var orgSender:ActorRef = _
+
   implicit val timeout: Timeout = 5 seconds
 
   override def receive: Receive = {
     case RegistrateUser(user, address) => {
-      val p = Promise[Boolean]
-      (userService ? RegistrateUser(user, address))
-        .mapTo[UserRegistrated] onComplete {
-        case Success(s) => {
-          log.debug("Registrate user - success!")
-          jdbcConActor ! Commit
-          p.success(true)
-        }
-        case Failure(f) => {
-          log.error(f, f.getMessage)
-          p.failure(f)
-          jdbcConActor ! Rollback
-          self ! PoisonPill
-        }
-      }
-      p.future pipeTo sender
+      orgSender = sender
+      userService ! RegistrateUser(user, address)
+      //      val p = Promise[Boolean]
+      //      (userService ? RegistrateUser(user, address))
+      //        .mapTo[UserRegistrated] onComplete {
+      //        case Success(s) => {
+      //          log.debug("Registrate user - success!")
+      //          jdbcConActor ! Commit
+      //          p.success(true)
+      //        }
+      //        case Failure(f) => {
+      //          log.error(f, f.getMessage)
+      //          p.failure(f)
+      //          jdbcConActor ! Rollback
+      //          self ! PoisonPill
+      //        }
+      //      }
+      //      p.future pipeTo sender
+      //    }
+    }
+    case UserRegistrated() =>
+    {
+      log.debug("Registrate user - success!")
+      jdbcConActor ! Commit
+      orgSender ! true
+    }
+    case akka.actor.Status.Failure(e) =>
+    {
+      log.error(e, e.getMessage)
+      jdbcConActor ! Rollback
+      orgSender ! e
     }
 
   }
